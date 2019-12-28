25a4970fb3206e2f9ae9b724f48982fd64ca3fb6
luisma <luismanuel.fernandez.camacho@gmail.com>
2019-12-28 17:46:54 +0100

    feat: css style loader

---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');  /*se instancia el modulo de path de node*/
+
+/* El siguiente codigo en realidad es igual a lo siguiente */
+/* npx webpack  --entry ./index.js --output ./bundle.js --mode development */
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, 'src/js/index.js'),
+  },
+  mode: 'development',
+  output: {
+    /*direccion relativa de posicion actual*/
+    path: path.resolve(__dirname, 'dist'), 
+    /*nombre de bundel */
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
